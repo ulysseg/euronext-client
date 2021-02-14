82a9a6f1943b3dba3d1bc5bbc9e60345bd961463
Ulysse Goarant <ulysse.goarant@protonmail.com>
2021-02-14 21:41:30 +0100

    fix test after setting use strict

--- test/all.test.js
+++ test/all.test.js
@@ -1,31 +1,25 @@
 const expect = require('chai').expect
 const client = require('../lib/index')
 
-describe('Client Test', () => {
+describe('Client Test', function() {
+
+    describe('#getDetailedQuote()', function() {
+        it('should get the detaild quote of Adyen', async function() {
+            const quote = await client.getDetailedQuote('NL0012969182', 'XAMS');
+            expect(quote.instrumentName).to.equal('ADYEN');
+            expect(quote.instrumentPrice).to.be.greaterThan(0).and.be.finite;
+        });
+    });
+
+    describe('#getFullDetailedQuote()', function() {
+        it('should get the full detaild quote of Adyen', async function() {
+            const quote = await client.getFullDetailedQuote('NL0012969182', 'XAMS');
+            expect(quote._52weekLow).to.be.greaterThan(0).and.be.finite;
+            expect(quote._52weekHigh).to.be.greaterThan(0).and.be.finite;
+            expect(quote._52weekLow).to.be.lessThan(quote._52weekHigh);
+        });
+    });    
 
     // Another test with 'FR0000133308', 'XPAR'?
-
-    describe('#getDetailedQuote()', () => {
-        it('should get the detaild quote of Adyen', (done) => {
-            const quotePromise = client.getDetailedQuote('NL0012969182', 'XAMS')
-            quotePromise.then(q => {
-                expect(q.instrumentName).to.equal('ADYEN');
-                expect(q.instrumentPrice).to.be.greaterThan(0).and.be.finite;
-                done();
-            });
-        });
-    });
-
-    describe('#getFullDetailedQuote()', () => {
-        it('should get the full detaild quote of Adyen', (done) => {
-            const quotePromise = client.getFullDetailedQuote('NL0012969182', 'XAMS')
-            quotePromise.then(q => {
-                expect(q._52weekLow).to.be.greaterThan(0).and.be.finite;
-                expect(q._52weekHigh).to.be.greaterThan(0).and.be.finite;
-                expect(q._52weekLow).to.be.lessThan(q._52weekHigh);
-                done();
-            });
-        });
-    });
  
 });
